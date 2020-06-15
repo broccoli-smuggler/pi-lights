4868659c6db51d1c2893a40e6becd976da40703c
Joshua <joshua.jordan@reaktor.com>
2020-06-15 20:33:18 +0200

    test pattern

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 from display import OpenCVDisplay
 from pi.pixel_display import PixelDisplay
 
-# d = PixelDisplay(15)
-d = OpenCVDisplay(15)
-d.add_dir("Sky")
+d = PixelDisplay(15)
+# d = OpenCVDisplay(15)
+# d.add_dir("Sky")
 # d.add_dir("crown", ".")
-# d.add_image("t.png")
+d.add_image("t.png")
 
 # d.add_image("wave.jpg")
 # d.add_images(["1.png", "2.png", "3.png", "4.png", "3.png", "2.png"])
