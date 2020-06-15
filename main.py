04362561c2877d80a35bcfe3f1af00bf1aed4910
broccoli-smuggler <joshuansjordan@gmail.com>
2020-06-15 22:28:00 +0100

    working on small!

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ from pi.pixel_display import PixelDisplay
 
 d = PixelDisplay(15)
 # d = OpenCVDisplay(15)
-# d.add_dir("Sky")
-# d.add_dir("crown", ".")
-d.add_image("t.png")
+d.add_dir("Sky")
+#d.add_dir("crown", ".")
+#d.add_image("t.png")
 
 # d.add_image("wave.jpg")
-# d.add_images(["1.png", "2.png", "3.png", "4.png", "3.png", "2.png"])
 d.display()
--- pi/pixel_display.py
+++ pi/pixel_display.py
@@ -23,6 +23,10 @@ if platform.system() != 'Darwin':
             # NeoPixels must be connected to D10, D12, D18 or D21 to work.
             pixel_pin = board.D18
 
+            if diameter == 15:
+                self._adjust_for_small_light()
+                print("Display really initialised with {} pixels".format(self.get_number_pixels()))
+            
             # The number of NeoPixels
             num_pixels = self.get_number_pixels()
 
@@ -31,20 +35,26 @@ if platform.system() != 'Darwin':
             self.ORDER = neopixel.GRB
 
             self.pixels = neopixel.NeoPixel(
-                pixel_pin, num_pixels, brightness=0.4, auto_write=False, pixel_order=self.ORDER
+                pixel_pin, num_pixels, brightness=0.05, auto_write=False, pixel_order=self.ORDER
             )
             self.pixels.fill(0)
             self.pixels.show()
             time.sleep(0.5)
 
-        def _convert_to_pixel_array(self, mat):
-            # Convert from BGR to GRB
-            mat[:, :, [0, 1, 2]] = mat[:, :, [2, 0, 1]]
-
-            # For our array we actually start with a 7 row
+        def _adjust_for_small_light(self):
+            # For our array we start with a 7 row
+            # And end with a 6 row 
             print("Adjusting circle mask for small light")
             self._circle_mask[0,4] = 255
             self._circle_mask[0,10] = 255
+            self._circle_mask[14,4] = 255
+            self._circle_mask[14,9] = 255
+
+        def _convert_to_pixel_array(self, mat):
+            # Convert from BGR to GRB
+            mat[:, :, [0, 1, 2]] = mat[:, :, [2, 1, 0]]
+
+
 
             # Convert to an array of rgb
             mat = mat[self._circle_mask.nonzero()]
